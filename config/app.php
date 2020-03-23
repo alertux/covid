c2f424aa329da71ca2e97241f69dbdc182fd05d5
wilfredonoyola <wilfredon163@gmail.com>
2020-03-23 15:30:14 -0600

    Removed APP_URL & ASSET_URL as default to environment var from ".env" file.

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://covidappdev.alertux.com'),
+    'url' => env('APP_URL'),
 
-    'asset_url' => env('ASSET_URL', 'http://covidappdev.alertux.com'),
+    'asset_url' => env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
